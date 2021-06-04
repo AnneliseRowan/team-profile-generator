73055d1c18dff919895d872a0afcf62ad8ac7054
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-03 22:52:46 -0400

    fixed the bug and got hmtl to render

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const { TestWatcher } = require("@jest/core");
 const Employee = require("../lib/employee"); 
 
-TestWatcher("Can instantiate Employee instance", () => {
+describe("Can instantiate Employee instance", () => {
     const e = new Employee(); 
     expect(typeof(e)).toBe("object");
 })
