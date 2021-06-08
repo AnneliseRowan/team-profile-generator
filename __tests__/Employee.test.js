19c5976144bb63596f1e78446cbf8ac4ca197038
Annélise Rowan <40217057+AnneliseRowan@users.noreply.github.com>
2021-06-08 18:27:44 -0400

    adding more tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,28 @@
 const Employee = require("../lib/employee"); 
 
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof(e)).toBe("object");
+  });
+  
+test("Can set name via constructor arguments", () => {
+    const name = "Annelise";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Annelise", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+    const testValue = "Annelise@test.com";
+    const e = new Employee("Annelise", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
 test (`Can get user name by getName()`, () => {
     const test = "Annelise Rowan"; 
     const emp = new Employee (test);
